c60e6856b2f024b9568ca57c849608153373c929
eclimov <klimovedik88@gmail.com>
2025-02-14 14:10:46 +0200

    - Added sidebar - Added MUI Icons

--- src/apps/AppInventoryList.tsx
+++ src/apps/AppInventoryList.tsx
@@ -1,7 +1,9 @@
-import {DataGrid, GridColDef} from '@mui/x-data-grid';
+import {DataGrid, GridCellParams, GridColDef} from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import slackLogo from '/slack.png'
 import AppInventoryListItemName from "./AppInventoryListItemName.tsx";
+import { useState } from 'react';
+import Sidebar from "./Sidebar.tsx";
 
 const columns: GridColDef[] = [
   { field: 'name', headerName: 'Name', width: 140, sortable: true, renderCell: (params) => <AppInventoryListItemName name={params.value} logoSrc={params.row.logoSrc} /> },
@@ -9,7 +11,7 @@ const columns: GridColDef[] = [
   { field: 'connector', headerName: 'Connector', width: 140, renderCell: (params) => <>TBD</> },
 ];
 
-interface AppInventoryItemType {
+export interface AppInventoryItemType {
   id: number
   logoSrc: string
   name: string
@@ -24,14 +26,27 @@ const rows: AppInventoryItemType[] = [
 const paginationModel = { page: 0, pageSize: 25 };
 
 function AppInventoryList() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<AppInventoryItemType>(rows[0]);
+
+  const openDrawer = (params: GridCellParams) => {
+    setSelectedItem(params.row)
+    setDrawerOpen((drawerOpen) => !drawerOpen);
+  }
+
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  }
 
   return (
     <Paper elevation={0}>
+      <Sidebar open={drawerOpen} onClose={closeDrawer} item={selectedItem as AppInventoryItemType} />
+
       <DataGrid
         rows={rows}
         getRowHeight={() => 'auto'}
         columns={columns}
-        onCellClick={() => alert('clicked')}
+        onCellClick={(params) => openDrawer(params)}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[25, 50]}
         sx={{ border: 0 }}
